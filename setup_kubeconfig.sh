f661342dd68d75c3a531e65903ab76430ce60afa
Ricardo Oliveira <ricardo@hashicorp.com>
2020-12-14 16:08:17 +0000

    Update setup_kubeconfig.sh

--- setup_kubeconfig.sh
+++ setup_kubeconfig.sh
@@ -2,11 +2,11 @@
 
 gcloud container clusters get-credentials \
     $(terraform output -json kubernetes_clusters | jq -r .[0].kubernetes_cluster_name) \
-    --region $(terraform output region)
+    --region $(terraform output -json region | jq -r)
 
 gcloud container clusters get-credentials \
     $(terraform output -json kubernetes_clusters | jq -r .[1].kubernetes_cluster_name) \
-    --region $(terraform output region)
+    --region $(terraform output -json region | jq -r)
 
 # gcloud container clusters get-credentials \
 #     $(terraform output -json kubernetes_clusters | jq -r .[2].kubernetes_cluster_name) \
