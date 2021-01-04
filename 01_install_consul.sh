7a203f26f8960c0b002be752b932b1e11efc04b8
Ricardo Oliveira <ricardo@hashicorp.com>
2021-01-04 10:19:20 +0000

    install consul

---
+++ 01_install_consul.sh
@@ -0,0 +1,68 @@
+#! /bin/bash
+source setup_kubeconfig.sh
+source helper.sh
+
+## setting up DNS first
+# c1_kctl apply -f dns/ExternalDNS-cluster-1.yaml
+# c2_kctl apply -f dns/ExternalDNS-cluster-2.yaml
+
+# setting up gossip
+consul keygen > gossipEncryptionKey
+
+CONSUL_HELM_VERSION=0.27.0
+helm repo add hashicorp https://helm.releases.hashicorp.com
+helm repo update
+c1_kctx
+kubectl create secret generic consul-gossipencryptionkey --from-file=key=gossipEncryptionKey
+if helm status consul 2>&1 1>/dev/null; then
+helm upgrade consul hashicorp/consul -f values1_consul.yaml \
+    --set global.datacenter=cluster-1 \
+    --version $CONSUL_HELM_VERSION \
+    --wait
+else
+helm install consul hashicorp/consul -f values1_consul.yaml \
+    --set global.datacenter=cluster-1 \
+    --version $CONSUL_HELM_VERSION \
+    --wait
+fi
+kubectl wait --for=condition=available --timeout=1m deployment.apps/consul-mesh-gateway
+
+kubectl get secret consul-federation -o yaml > consul-federation-secret.yaml
+kubectl get secrets consul-acl-replication-acl-token -o yaml > consul-acl-replication-acl-token.yaml
+echo ".: waiting for Cluster-1 IP address"
+while ! kubectl get svc consul-ui -o jsonpath={..ip} --allow-missing-template-keys=false 2>/dev/null; do
+    sleep 5
+done
+echo ""
+
+c2_kctx
+kubectl create secret generic consul-gossipencryptionkey --from-file=key=gossipEncryptionKey
+if helm status consul 2>&1 1>/dev/null; then
+helm upgrade consul hashicorp/consul -f values2_consul.yaml \
+    --set global.datacenter=cluster-2 \
+    --version $CONSUL_HELM_VERSION \
+    --wait
+else
+kubectl apply -f consul-federation-secret.yaml
+kubectl apply -f consul-acl-replication-acl-token.yaml
+
+helm install consul hashicorp/consul -f values2_consul.yaml \
+    --set global.datacenter=cluster-2 \
+    --version $CONSUL_HELM_VERSION \
+    --wait
+fi
+kubectl wait --for=condition=available --timeout=1m deployment.apps/consul-mesh-gateway
+echo ".: waiting for Cluster-2 IP address"
+while ! kubectl get svc consul-ui -o jsonpath={..ip} --allow-missing-template-keys=false 2>/dev/null; do
+    sleep 5
+done
+echo ""
+
+setup-consul 2>&1 1>/dev/null
+
+echo ".: adding Consul license"
+consul license put -datacenter=cluster-1 @$HOME/Documents/consul_v2lic.hclic
+consul license put -datacenter=cluster-2 @$HOME/Documents/consul_v2lic.hclic
+
+detect_endpoints;
+c1_kctx;
--- 99_cleanup.sh
+++ 99_cleanup.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 source helper.sh
+
 c1_kctx
 helm uninstall vault
 kubectl delete $(kubectl get pvc -o name | grep vault | tr '\n' ' ')
@@ -25,26 +26,27 @@ rm -f *.key
 rm -f *.crt
 rm -f keys*
 
-# c1_kctx
-# helm uninstall cluster-1
-# helm uninstall prometheus
-# helm uninstall grafana
-# kubectl delete -f c1_manifests/
-# kubectl delete $(kubectl get pvc -o name | tr '\n' ' ')
-# kubectl delete $(kubectl get sa -o name | grep consul)
-# kubectl delete $(kubectl get secrets -o name | grep consul)
+c1_kctx
+helm uninstall consul
+helm uninstall prometheus
+helm uninstall grafana
+kubectl delete -f c1_manifests/
+kubectl delete -f bookinfo/bookinfo.yaml
+kubectl delete $(kubectl get pvc -o name | grep consul| tr '\n' ' ')
+kubectl delete $(kubectl get sa -o name | grep consul)
+kubectl delete $(kubectl get secrets -o name | grep consul)
 
-# c2_kctx
-# helm uninstall cluster-2
-# kubectl delete -f consul-federation-secret.yaml
-# kubectl delete -f consul-acl-replication-acl-token.yaml
-# kubectl delete -f c2_manifests/
-# kubectl delete $(kubectl get pvc -o name | tr '\n' ' ')
-# kubectl delete $(kubectl get sa -o name | grep consul)
-# kubectl delete $(kubectl get secrets -o name | grep consul)
+c2_kctx
+helm uninstall consul
+kubectl delete -f consul-federation-secret.yaml
+kubectl delete -f consul-acl-replication-acl-token.yaml
+kubectl delete -f c2_manifests/
+kubectl delete $(kubectl get pvc -o name | grep consul| tr '\n' ' ')
+kubectl delete $(kubectl get sa -o name | grep consul)
+kubectl delete $(kubectl get secrets -o name | grep consul)
 
 # c3_kctl delete -f c3_manifests/
 
-# rm -f consul-federation-secret.yaml
-# rm -f consul-acl-replication-acl-token.yaml
-# rm -f external-counter.json
+rm -f consul-federation-secret.yaml
+rm -f consul-acl-replication-acl-token.yaml
+rm -f external-counter.json
